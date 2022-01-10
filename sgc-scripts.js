513033ec774a62b575953d495f5cbf0cdf0b479f
Maxime Avranche <avranche.maxime@gmail.com>
2022-01-10 14:00:51 +0100

    Exercice jQuery

--- sgc-scripts.js
+++ sgc-scripts.js
@@ -6,17 +6,39 @@
 jQuery(document).ready(function(){
 
 	// Déclarer action déclenchement
-	jQuery('.js-btn').click(function(event) {
+	jQuery('.js-btn').click(function( event ){
 		// Désactive comportement du clic
 		event.preventDefault();
+
 		// Au clique du btn
 		var sgc_recup = jQuery(this).attr('id');
+
 		// Teste de recupération
 		//alert(sgc_recup);
 		// Transforme id en class + recup content
 		var sgc_content = jQuery('.' + sgc_recup).html();
 		jQuery('.js-content').html(sgc_content);
 
+		// Remonte tous les éléments
+		jQuery('.js-content').stop().slideUp('fast', function(){
+			// Montre élément choisi
+			jQuery('.js-content').stop().slideDown();
+		});
+		
+
+		
+		
+		// Affiche le texte en slide down & up
+		//jQuery('.js-content').stop().slideToggle();
+
+		// Ajouter une class "active"
+		// Slide up des contenus masqués
+		//jQuery('.js-content'.not('.active')).slideUp();
 	});
 
+});
+
+
+jQuery(document).ready(function(){
+	
 });
