9bc21a62f0fafc175c9f088a3625936291b8a847
Maxime Avranche <avranche.maxime@gmail.com>
2022-01-10 16:54:44 +0100

    Création d'un quizz

--- sgc-scripts.js
+++ sgc-scripts.js
@@ -36,9 +36,67 @@ jQuery(document).ready(function(){
 		//jQuery('.js-content'.not('.active')).slideUp();
 	});
 
+
+
+
+/**
+  * Message "Try it"
+**/
+	// Cible le input en question
+	jQuery('input').focus(function(){
+		// Le prochain span (suivant l'input) va apparaître durant un délais défini et prendre les paramètres css
+		jQuery(this).next('span').css('display', 'inline').fadeOut(1500);
+	});
+
+
+
+/**
+  * Message quand la saisie est en cours
+**/
+// Page chargée
+
+	// Lorsque l'utilisateur saisi l'input, le message s'affichera
+	jQuery('.sgc-answer').keyup(function(){
+		// Value à récupérer
+		var sgc_txt_saisi = "Test123";
+		if(jQuery('#sgc-answer-id').val() == jQuery(sgc_txt_saisi).val()) {
+				alert("Correspond");
+			}
+		// Afficher après le champs input un texte qui apparaît
+		jQuery(this).next().html("<span class='sgc-pop-try'>Essaies encore pour voir...</div>").fadeIn();
+
+	});
+
+
+/**
+  * Cases à cocher
+**/
+// Quand cliqué
+jQuery(document).click(function(){
+	// On vérifie si la bonne réponse (définie ici) est 'checked'
+	if(jQuery('input[name=merci]').is(':checked')) {
+		// Affichage d'un message félicitant l'utilisateur + PASSER A L'AUTRE QUESTION
+		jQuery('.sgc-q2-error').stop().slideUp();
+		jQuery('#sgc-quizz-q3').css('display', 'inline').slideDown();
+	}
+	else {
+		// Message indiquant qu'il ne s'agit pas de la bonne réponse
+		jQuery('.sgc-q2-error').text("Mauvaise réponse ! Tu as de la chance, je suis de bonne humeur, tu peux retenter ta chance.").stop().slideDown();
+		jQuery('#sgc-quizz-q3').fadeOut(0.1);
+	}
 });
 
 
-jQuery(document).ready(function(){
+/**
+  * Bouton "Devenir riche"
+**/
+
+	jQuery('#joke').click(function(){
+		// Désactive comportement du clic
+		event.preventDefault();
+		// Animation du btn
+		jQuery('.sgc-btn-quizz').animate({"marginLeft": "+=250px", "marginTop": "+=50px"}, "fast");
+	});
+
 	
 });
